c6248dc29fbbf64f27dddabb262b03e4bd3fcba3
bambangsubambang <148016312+bambangsubambang@users.noreply.github.com>
2025-04-22 07:47:06 +0700

    Update inference_1b.py

--- inference_1b.py
+++ inference_1b.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/19RbeqMcQwazIlcg5uFcOTSSfoxn5F3vm
 """
 
-!pip install streamlit
-
 import streamlit as st
 import numpy as np
 import pandas as pd
